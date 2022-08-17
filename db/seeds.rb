2d219d404db3ff726c6ab1882b494f17c4efde46
Clari Ursini <clari_ursini@hotmail.com>
2022-08-17 14:38:49 -0300

    Finished app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Clean DB'
+Restaurant.destroy_all
+
+puts 'Creating Restaurants'
+
+Restaurant.create(name: 'El cuartito', address: 'Talcahuano', phone_number: '4953-1391', category: 'italian')
+Restaurant.create(name: 'Guerrin', address: 'Corrientes', phone_number: '4813-4202', category: 'italian')
+Restaurant.create(name: 'Osaka', address: 'Palermo', phone_number: '4804-7400', category: 'japanese')
+Restaurant.create(name: 'Merci', address: 'Carlos Calvo', phone_number: '4811-5598', category: 'french')
+Restaurant.create(name: 'Yong Xing', address: 'Combate de los Pozos', phone_number: '4805-1789', category: 'chinese')
+
+puts 'Done'
